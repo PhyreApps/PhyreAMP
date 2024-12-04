b79b6fbc6760c51615f4f83668231cf6d3dbd034
Bozhidar <bobicloudvision@gmail.com>
2024-12-04 22:19:56 +0200

    update

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,11 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-import Button from './Component/Button.jsx';
 import VirtualHostForm from './Component/VirtualHostForm.jsx';
 import VirtualHostTable from './Component/VirtualHostTable.jsx';
 
 const root = createRoot(document.getElementById('app'));
 root.render(
     <>
-        <h1>Hello, Electron!</h1>
-        <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
-        <Button onClick={() => alert('Button clicked!')}>Click me!</Button>
         <VirtualHostForm />
         <VirtualHostTable />
     </>
