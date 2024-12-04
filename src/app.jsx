38a855aaca13f0a94d900d24493289faa17d1676
Bozhidar <bobicloudvision@gmail.com>
2024-12-04 20:51:37 +0200

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -2,5 +2,13 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.getElementById('app'));
-root.render(<h2>Hello from React!</h2>);
+root.render(
+    <>
+        <h1>Hello, Electron!</h1>
+        <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
+        <div>
+            <button>Click me!</button>
+        </div>
+    </>
+);
 
