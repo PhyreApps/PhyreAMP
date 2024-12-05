0c2b5f1c17c790174a5a8cbbb28c1be78d8c4b5b
Bozhidar <bobi@microweber.com>
2024-12-05 23:49:00 +0200

    Update forge.config.js

--- forge.config.js
+++ forge.config.js
@@ -4,7 +4,8 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
-    icon: './src/assets/icons/icon.icns'
+    icon: './src/assets/icons/icon.icns',
+    arch: "universal",
   },
   rebuildConfig: {},
   publishers: [
