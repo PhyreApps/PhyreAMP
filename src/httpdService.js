3a613d4c28e6037cbbc82de4a7e01975f5bb5672
Bozhidar <bobi@microweber.com>
2025-01-24 17:36:23 +0200

    Update httpdService.js

--- src/httpdService.js
+++ src/httpdService.js
@@ -4,10 +4,10 @@ const Docker = require('dockerode');
 const path = require("node:path");
 import { getSettings, getVirtualHosts } from './database.js';
 import {app} from "electron";
-var docker = new Docker();
 
 const startHttpdContainer = async () => {
     try {
+        var docker = new Docker();
         const container = docker.getContainer('phyreamp-httpd');
         await container.start();
         return { success: true, message: 'HTTPD container started successfully.' };
@@ -18,6 +18,7 @@ const startHttpdContainer = async () => {
 
 const stopHttpdContainer = async () => {
     try {
+        var docker = new Docker();
         const container = docker.getContainer('phyreamp-httpd');
         await container.stop();
         return { success: true, message: 'HTTPD container stopped successfully.' };
@@ -28,6 +29,7 @@ const stopHttpdContainer = async () => {
 
 const restartHttpdContainer = async () => {
     try {
+        var docker = new Docker();
         const container = docker.getContainer('phyreamp-httpd');
         await container.restart();
         return { success: true, message: 'HTTPD container restarted successfully.' };
@@ -38,6 +40,7 @@ const restartHttpdContainer = async () => {
 
 const getHttpdContainerStatus = async () => {
     try {
+        var docker = new Docker();
         const container = docker.getContainer('phyreamp-httpd');
         const data = await container.inspect();
         if (data.State.Running) {
@@ -50,6 +53,12 @@ const getHttpdContainerStatus = async () => {
 };
 
 const createHttpdContainer = async () => {
+
+    var docker = new Docker();
+    docker.ping(function(err, data) {
+        console.log('docker.ping:', err, data);
+    });
+
     try {
         const container = docker.getContainer('phyreamp-httpd');
         const data = await container.inspect();
@@ -57,74 +66,77 @@ const createHttpdContainer = async () => {
             return { success: true, message: 'HTTPD container already exists.' };
         }
     } catch (error) {
-        if (error.statusCode === 404) {
-            try {
-                await new Promise((resolve, reject) => {
-                    docker.pull('httpd:latest', (err, stream) => {
+
+        console.log('createHttpdContainer error:', error);
+
+        try {
+            await new Promise((resolve, reject) => {
+                docker.pull('httpd:latest', (err, stream) => {
+                    if (err) {
+                        return reject(new Error(`Error pulling HTTPD image: ${err.message}`));
+                    }
+                    docker.modem.followProgress(stream, onFinished, onProgress);
+
+                    function onFinished(err, output) {
                         if (err) {
                             return reject(new Error(`Error pulling HTTPD image: ${err.message}`));
                         }
-                        docker.modem.followProgress(stream, onFinished, onProgress);
+                        resolve();
+                    }
 
-                        function onFinished(err, output) {
-                            if (err) {
-                                return reject(new Error(`Error pulling HTTPD image: ${err.message}`));
-                            }
-                            resolve();
-                        }
-
-                        function onProgress(event) {
-                            console.log(event);
-                        }
-                    });
-                });
-                const settings = await getSettings();
-
-                const virtualHosts = await getVirtualHosts();
-                const binds = virtualHosts.map(host => `${host.project_path}:/var/www/html/${host.local_domain}`);
-
-
-                const userDataPath = app.getPath('userData');
-                const apacheDataPath = path.join(userDataPath, 'apache');
-
-                const newApacheConfigPath = path.join(apacheDataPath, 'httpd.conf');
-                if (fs.existsSync(newApacheConfigPath)) {
-                    binds.push(`${newApacheConfigPath}:/usr/local/apache2/conf/httpd.conf`);
-                }
-
-                const newApacheVirtualHostsPath = path.join(apacheDataPath, 'virtualhosts.conf');
-                if (fs.existsSync(newApacheVirtualHostsPath)) {
-                    binds.push(`${newApacheVirtualHostsPath}:/usr/local/apache2/conf/virtualhosts.conf`);
-                }
-
-                console.log('binds:', binds);
-
-                const extraHosts = virtualHosts.map(host => `${host.local_domain}:127.0.0.1`);
-
-                const container = await docker.createContainer({
-                    Image: 'httpd:latest',
-                    name: 'phyreamp-httpd',
-                    HostConfig: {
-                        NetworkMode: 'phyreamp-network',
-                        PortBindings: {
-                            '80/tcp': [{ HostPort: (settings.httpdPort || '80').toString() }]
-                        },
-                        Binds: binds,
-                        ExtraHosts: extraHosts
+                    function onProgress(event) {
+                        console.log(event);
                     }
                 });
-                await container.start();
-                return { success: true, message: 'HTTPD container created and started successfully.' };
-            } catch (createError) {
-                return { success: false, error: `Error creating HTTPD container: ${createError.message}` };
+            });
+            const settings = await getSettings();
+
+            const virtualHosts = await getVirtualHosts();
+            const binds = virtualHosts.map(host => `${host.project_path}:/var/www/html/${host.local_domain}`);
+
+
+            const userDataPath = app.getPath('userData');
+            const apacheDataPath = path.join(userDataPath, 'apache');
+
+            const newApacheConfigPath = path.join(apacheDataPath, 'httpd.conf');
+            if (fs.existsSync(newApacheConfigPath)) {
+                binds.push(`${newApacheConfigPath}:/usr/local/apache2/conf/httpd.conf`);
             }
+
+            const newApacheVirtualHostsPath = path.join(apacheDataPath, 'virtualhosts.conf');
+            if (fs.existsSync(newApacheVirtualHostsPath)) {
+                binds.push(`${newApacheVirtualHostsPath}:/usr/local/apache2/conf/virtualhosts.conf`);
+            }
+
+            console.log('binds:', binds);
+
+            const extraHosts = virtualHosts.map(host => `${host.local_domain}:127.0.0.1`);
+
+            const container = await docker.createContainer({
+                Image: 'httpd:latest',
+                name: 'phyreamp-httpd',
+                HostConfig: {
+                    NetworkMode: 'phyreamp-network',
+                    PortBindings: {
+                        '80/tcp': [{ HostPort: (settings.httpdPort || '80').toString() }]
+                    },
+                    Binds: binds,
+                    ExtraHosts: extraHosts
+                }
+            });
+            await container.start();
+            return { success: true, message: 'HTTPD container created and started successfully.' };
+        } catch (createError) {
+            return { success: false, error: `Error creating HTTPD container: ${createError.message}` };
         }
+
         return { success: false, error: `Error checking HTTPD container: ${error.message}` };
     }
 };
 
 const deleteHttpdContainer = async () => {
     try {
+        var docker = new Docker();
         const container = docker.getContainer('phyreamp-httpd');
         await container.remove({ force: true });
         return { success: true, message: 'HTTPD container deleted successfully.' };
