45d35e913747b42e8f172e0e3bcf5fb0e6c8d418
Bozhidar Slaveykov <bobi@microweber.com>
2025-01-28 13:55:27 +0200

    update

---
+++ docker/php/73/publish-and-build.sh
@@ -0,0 +1,2 @@
+docker build -t selfworks/phyreamp-php7.3:latest .
+docker push selfworks/phyreamp-php7.3:latest
---
+++ docker/php/83/publish-and-build.sh
@@ -0,0 +1,2 @@
+docker build -t selfworks/phyreamp-php8.3:latest .
+docker push selfworks/phyreamp-php8.3:latest
