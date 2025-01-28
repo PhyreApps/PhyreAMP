7f5064065c4dfd9ed61be245a0551e978ecd2953
Bozhidar Slaveykov <bobi@microweber.com>
2025-01-28 12:37:21 +0200

    Create index.php

---
+++ test-apps/php/index.php
@@ -0,0 +1,34 @@
+<?php
+
+$checkFunctionsThatAreExist = [
+    'phpinfo',
+    'phpversion',
+    'php_uname',
+    'php_sapi_name',
+    'php_ini_loaded_file',
+    'mysql_connect',
+    'mysqli_connect',
+    'curl_init',
+    'gd_info',
+    'mysqli_fetch_all',
+    'mysqli_connect'
+];
+
+foreach ($checkFunctionsThatAreExist as $function) {
+    if (function_exists($function)) {
+        echo '<span class="green">' . $function . ' exists' . "</span>" . "<br />";
+    } else {
+        echo '<span class="red">' . $function . ' does not exist' . "</span>" . "<br />";
+    }
+}
+
+?>
+
+<style>
+    .red {
+        color: red;
+    }
+    .green {
+        color: green;
+    }
+</style>
